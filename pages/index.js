9f7510660995d3f45b09a6e983b731d177ea8afd
Devon Lambert <devon@realityknights.com>
2019-06-20 16:33:41 -0500

    Client side pretty urls

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const PostLink = props => (
     <li>
-        <Link href={`/post?title=${props.title}`}>
+        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
             <a>{props.title}</a>
         </Link>
     </li>
@@ -13,9 +13,9 @@ const Home = () => (
     <Layout>
         <h1>Devon's Blog</h1>
         <ul>
-            <PostLink title="Hello Next.js" />
-            <PostLink title="Learn Why Next.js is awesome" />
-            <PostLink title="Deploy apps with Zeit" />
+            <PostLink id="hello-nextjs" title="Hello Next.js" />
+            <PostLink id="learn-nextjs" title="Learn Why Next.js is awesome" />
+            <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
         </ul>
     </Layout>
 );
