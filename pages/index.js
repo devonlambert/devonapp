3e3af5d30d569feb9e82205bd3f7642fba9114d2
Devon Lambert <devon@realityknights.com>
2019-06-20 16:30:29 -0500

    Dynamic routing

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import Layout from '../components/Layout';
+import Link from 'next/link';
+
+const PostLink = props => (
+    <li>
+        <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+);
 
 const Home = () => (
     <Layout>
-        <p>Welcome to Devon's World!</p>
+        <h1>Devon's Blog</h1>
+        <ul>
+            <PostLink title="Hello Next.js" />
+            <PostLink title="Learn Why Next.js is awesome" />
+            <PostLink title="Deploy apps with Zeit" />
+        </ul>
     </Layout>
 );
 
