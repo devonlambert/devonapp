365bd24da46f4123a12b58555008ff55f69c0706
Devon Lambert <devon@realityknights.com>
2019-06-22 21:14:44 -0500

    TV Maze API fetching with getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,31 @@
 import Layout from '../components/Layout';
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-const PostLink = props => (
-    <li>
-        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-            <a>{props.title}</a>
-        </Link>
-    </li>
-);
-
-const Home = () => (
+const Index = props => (
     <Layout>
-        <h1>Devon's Blog</h1>
+        <h1>Batman TV Shows</h1>
         <ul>
-            <PostLink id="hello-nextjs" title="Hello Next.js" />
-            <PostLink id="learn-nextjs" title="Learn Why Next.js is awesome" />
-            <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+            {props.shows.map(show => (
+                <li key={show.id}>
+                    <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+                        <a>{show.name}</a>
+                    </Link>
+                </li>
+            ))}
         </ul>
     </Layout>
 );
 
-export default Home;
+Index.getInitialProps = async () => {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await res.json();
+
+    console.log(`Show data fetched. Count: ${data.length}`);
+
+    return {
+        shows: data.map(entry => entry.show)
+    };
+}
+
+export default Index;
